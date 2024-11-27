235a53518c3725e78b70382fb1e652f6d93166da
quocnhut1010 <128989079+quocnhut1010@users.noreply.github.com>
2024-11-27 10:34:28 +0700

    update them

--- app/src/main/java/vn/edu/tdmu/democloudcomputing/MainActivity.java
+++ app/src/main/java/vn/edu/tdmu/democloudcomputing/MainActivity.java
@@ -9,7 +9,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    //heheheheheh
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
